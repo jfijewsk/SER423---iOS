b36fd840061adc759bc67fe8229b8c1ae1e67095
James <james@Jamess-Mac.local>
2020-04-02 07:58:18 -0500

    Working with value pair

--- Assign6jfijewsk/Assign6jfijewsk/ViewController.swift
+++ Assign6jfijewsk/Assign6jfijewsk/ViewController.swift
@@ -17,8 +17,33 @@ class ViewController: UIViewController {
     
         getNames()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("viewWillAppear")
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        print("viewDidDisappear")
+    }
+    
+    
+    
+    
 
-
+    // Gets the names of the locations from the server
     func getNames(){
         var stub = StudentCollectionStub(urlString: "http://127.0.0.1:8080")
 
