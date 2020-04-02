e2f63163dac7ba6d7cad854460872eeed5384da3
James <james@Jamess-Mac.local>
2020-04-02 11:47:55 -0500

    Started to parse getnames

---
+++ Assign6jfijewsk/Assign6jfijewsk/edu/asu/bsse/jfijewsk/Assign6jfijewsk/PlaceTableViewController.swift
@@ -0,0 +1,138 @@
+//
+//  PlaceTableViewController.swift
+//  Assign6jfijewsk
+//
+//  Created by James on 4/2/20.
+//  Copyright © 2020 James. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class PlaceTableViewController: UITableViewController {
+
+    //var allPlaces:[(name: String)]
+    var urlAddress = ""
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Read in infoPlist values
+        readPropertyList()
+        getNames()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func readPropertyList() {
+        // Getting info plist as a dictionary
+        let dictionary = Bundle.main.infoDictionary
+        urlAddress = (dictionary!["ServerUrl"] as? String)!
+    }
+    
+    
+    // Gets the names of the locations from the server
+    func getNames(){
+        var result: [String]
+        var jsonResult = [String: Any]()
+        
+        let stub = StudentCollectionStub(urlString: urlAddress)
+        
+        let _:Bool = stub.getNames(callback: { (res: String, err: String?) -> Void in
+            if err != nil {
+                print("Error in getting names: \(String(describing: err))")
+            }else{
+
+                // Get string data and turn it into json
+                if let data = res.data(using: .utf8) {
+                    do {
+                        jsonResult = try (JSONSerialization.jsonObject(with: data, options: []) as? [String: Any])!
+                        print(jsonResult)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+                //return nil
+            }
+                
+                // no error, then the result, is a jsonrpc response with the value of result being an array of student names.
+                //print("jsonrpc response to getNames is: \(res)")
+            //}
+            
+            //return res
+            
+        })
+    }
+
+}
