99d793cb856af9fd2476cc883e13c5b949a189d1
James <james@Jamess-Mac.local>
2020-04-05 16:04:40 -0500

    Started on creating new place object

--- Assign6jfijewsk/Assign6jfijewsk/edu/asu/bsse/jfijewsk/Assign6jfijewsk/AddPlaceViewController.swift
+++ Assign6jfijewsk/Assign6jfijewsk/edu/asu/bsse/jfijewsk/Assign6jfijewsk/AddPlaceViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class AddPlaceViewController: UIViewController {
+    var urlAddress = ""
 
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var addressTitleText: UITextField!
@@ -25,6 +26,23 @@ class AddPlaceViewController: UIViewController {
     
     @IBOutlet weak var addBtn: UIButton!
     
+    @IBAction func saveAction(_ sender: Any) {
+        print("user hit save")
+        if(nameText.text!.isEmpty || descriptionText.text!.isEmpty || categoryText.text!.isEmpty){
+            let alert = UIAlertController(title: "Incomplete place description", message: "You must fill out the name description and category for a new place", preferredStyle: .alert)
+        
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+
+        
+            self.present(alert, animated: true)
+        }
+        
+        else{
+            // Make an async call to server and alert user to data being saved
+            readPropertyList()
+            
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -38,6 +56,57 @@ class AddPlaceViewController: UIViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func readPropertyList() {
+        // Getting info plist as a dictionary
+        let dictionary = Bundle.main.infoDictionary
+        urlAddress = (dictionary!["ServerUrl"] as? String)!
+    }
+    
+    
+        //init(name : String, description : String, category : String, address_title : String, address : String, elevation : Double, latitude : Double, longitude : Double)
+    func savePlace(){
+        let stub = PlaceCollectionStub(urlString: urlAddress)
+        
+        // Create new Place object
+        var address1 :String
+        var address2 : String
+        var address3 : String
+        
+        address1 = addressText1.text!
+        address2 = addressText2.text!
+        address3 = addressText3.text!
+        
+        var completeAddress = address1
+        completeAddress += "$"
+        completeAddress += address2
+        completeAddress += "$"
+        completeAddress += address3
+        
+        // Try to convert user entered fields to doubles
+        let parseElevation, parseLatitude, parseLongitude : Double
+        do{
+            
+        }
+        
+        catch{
+            print("Doubles were not entered correctly")
+        }
+            
+        //let newPlace = PlaceDescription(name: nameText.text!, description: descriptionText.text!, category: categoryText.text!, address_title: addressTitleText.text!, address: completeAddress, elevation: elevationText.text!, latitude: latitudeText.text!, longitude: longitudeText.text!)
+        
+        //let _:Bool = stub.add(callback: {Item: newPlace (res: String, err: String?) -> Void in
+            //if err != nil {
+                //print("Error saving place: \(String(describing: err))")
+           // }else{
+                
+                
+           // }
+            
+            
+       // })
+
     }
 
 
